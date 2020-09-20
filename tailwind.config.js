e8ed1966875683d42700fda31f078474911a1de0
Mauricio Etcheverry <mauricio@yoreparo.com>
2020-09-20 20:01:38 -0300

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './public/*html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#26BCD7'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
